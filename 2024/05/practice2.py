3756ecdcbb7b8ac13691d58c6734d0d243d54de6
Chad Armond <chad.l.armond@nasa.gov>
2024-12-06 08:09:45 -0600

    Day 5 solutions

---
+++ 2024/05/practice2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+page_ordering_rules = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+"""
+
+pages_to_produce = """
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+"""
+
+order = {}
+for line in page_ordering_rules.split("\n"):
+    if len(line) < 1:
+        continue
+    key, val = line.split("|")
+    if key not in order:
+        order[key] = []
+    order[key].append(val)
+
+total = 0
+for line in pages_to_produce.split("\n"):
+    if len(line) < 1:
+        continue
+    is_valid = True
+    pages = line.split(",")
+    for i in range(len(pages)):
+        if pages[i] in order:
+            rules = order[pages[i]]
+        else:
+            rules = []
+        for j in range(i):
+            curr_page = pages[j]
+            if curr_page in rules:
+                is_valid = False
+                break
+        if is_valid == False:
+            break
+    if is_valid == False:
+        for j in range(len(pages)):
+            for i in range(len(pages) - 1):
+                curr_page = pages[i]
+                next_page = pages[i+1]
+                if next_page in order and curr_page in order[next_page]:
+                    pages[i+1] = curr_page
+                    pages[i] = next_page
+        middle = int(len(pages) / 2)
+        total += int(pages[middle])
+
+print(f"Total is {total}")
---
+++ 2024/05/puzzle2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+with open("input1.txt", "r") as file:
+    page_ordering_rules = file.read()
+
+with open("input2.txt", "r") as file:
+    pages_to_produce = file.read()
+
+
+order = {}
+for line in page_ordering_rules.split("\n"):
+    if len(line) < 1:
+        continue
+    key, val = line.split("|")
+    if key not in order:
+        order[key] = []
+    order[key].append(val)
+
+total = 0
+for line in pages_to_produce.split("\n"):
+    if len(line) < 1:
+        continue
+    is_valid = True
+    pages = line.split(",")
+    for i in range(len(pages)):
+        if pages[i] in order:
+            rules = order[pages[i]]
+        else:
+            rules = []
+        for j in range(i):
+            curr_page = pages[j]
+            if curr_page in rules:
+                is_valid = False
+                break
+        if is_valid == False:
+            break
+    if is_valid == False:
+        for j in range(len(pages)):
+            for i in range(len(pages) - 1):
+                curr_page = pages[i]
+                next_page = pages[i+1]
+                if next_page in order and curr_page in order[next_page]:
+                    pages[i+1] = curr_page
+                    pages[i] = next_page
+        middle = int(len(pages) / 2)
+        total += int(pages[middle])
+
+print(f"Total is {total}")
